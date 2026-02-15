dd74b6aaaf9647ff1b2e98a7f987e61659f15e39
Stanislav Fishkov <fishkovs@yandex.ru>
2026-02-15 07:18:35 +0300

    add payment.server.url property

--- my-market-app-module/src/main/java/ru/yandex/practicum/mymarket/config/PaymentConfig.java
+++ my-market-app-module/src/main/java/ru/yandex/practicum/mymarket/config/PaymentConfig.java
@@ -1,19 +1,26 @@
 package ru.yandex.practicum.mymarket.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import ru.yandex.practicum.mymarket.payment.ApiClient;
 import ru.yandex.practicum.mymarket.payment.api.BalanceApi;
 import ru.yandex.practicum.mymarket.payment.api.PaymentApi;
 
 @Configuration
 public class PaymentConfig {
     @Bean
-    public BalanceApi balanceApi(){
-        return new BalanceApi();
+    public ApiClient apiClient(@Value("${payment.server.url}") String baseUrl){
+        return new ApiClient().setBasePath(baseUrl);
     }
 
     @Bean
-    public PaymentApi paymentApi(){
-        return new PaymentApi();
+    public BalanceApi balanceApi(ApiClient apiClient){
+        return new BalanceApi(apiClient);
+    }
+
+    @Bean
+    public PaymentApi paymentApi(ApiClient apiClient){
+        return new PaymentApi(apiClient);
     }
 }
